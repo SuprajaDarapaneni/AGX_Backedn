2ba52307243f83255a712821698bfc6ef77e44b6
yaswanthkumarch <yaswanthkumarch2001@gmail.com>
2025-06-07 07:01:25 +0530

    updated cors

--- index.js
+++ index.js
@@ -48,10 +48,25 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://www.agx-international.com',
+  'https://agx-international.com'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173', // Your Vite or React dev server
-  credentials: true,               // If you're using cookies or sessions
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
+
 app.use("/uploads", express.static("public/uploads"));
 
 /* ROUTES */
